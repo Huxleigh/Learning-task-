8e6b6637cd40e879b4cf2127983f60a934357d94
Huxleigh <99906794+Huxleigh@users.noreply.github.com>
2022-04-04 22:51:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
   <ul>
     <li><a href="index.php" target="_blank"> HOME </a></li>
     <li><a href="/LT2/Homepage.html" target= "_blank"> LEARNING TASK 2 </a> </li>
-    <li><a href="/lt3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+    <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+    <li><a href="/LT4/index.html" target= "_blank"> LEARNING TASK 4 </a> </li>
     <li><a href="/CLE1MT/index.html" target= "_blank"> CLE MIDTERM </a> </li>
   </ul>
   </body>
